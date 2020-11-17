c79b8f237a47203f50d24bd1098baf6fcca6d2af
antoinedlp <antdelplanque@gmail.com>
2020-11-17 15:24:54 +0100

    ref #4

---
+++ src/Entity/Sorties.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortiesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SortiesRepository::class)
+ */
+class Sorties
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datedebut;
+
+    /**
+     * @ORM\Column(type="time", nullable=true)
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datecloture;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbinscriptionsmax;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $descriptioninfos;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $etatsortie;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $urlPhoto;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $organisateur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDatedebut(): ?\DateTimeInterface
+    {
+        return $this->datedebut;
+    }
+
+    public function setDatedebut(\DateTimeInterface $datedebut): self
+    {
+        $this->datedebut = $datedebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?\DateTimeInterface
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?\DateTimeInterface $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDatecloture(): ?\DateTimeInterface
+    {
+        return $this->datecloture;
+    }
+
+    public function setDatecloture(\DateTimeInterface $datecloture): self
+    {
+        $this->datecloture = $datecloture;
+
+        return $this;
+    }
+
+    public function getNbinscriptionsmax(): ?int
+    {
+        return $this->nbinscriptionsmax;
+    }
+
+    public function setNbinscriptionsmax(int $nbinscriptionsmax): self
+    {
+        $this->nbinscriptionsmax = $nbinscriptionsmax;
+
+        return $this;
+    }
+
+    public function getDescriptioninfos(): ?string
+    {
+        return $this->descriptioninfos;
+    }
+
+    public function setDescriptioninfos(?string $descriptioninfos): self
+    {
+        $this->descriptioninfos = $descriptioninfos;
+
+        return $this;
+    }
+
+    public function getEtatsortie(): ?int
+    {
+        return $this->etatsortie;
+    }
+
+    public function setEtatsortie(?int $etatsortie): self
+    {
+        $this->etatsortie = $etatsortie;
+
+        return $this;
+    }
+
+    public function getUrlPhoto(): ?string
+    {
+        return $this->urlPhoto;
+    }
+
+    public function setUrlPhoto(?string $urlPhoto): self
+    {
+        $this->urlPhoto = $urlPhoto;
+
+        return $this;
+    }
+
+    public function getOrganisateur(): ?int
+    {
+        return $this->organisateur;
+    }
+
+    public function setOrganisateur(int $organisateur): self
+    {
+        $this->organisateur = $organisateur;
+
+        return $this;
+    }
+}
