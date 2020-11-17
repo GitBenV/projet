0d3569b913cb50ec050575ff2b8d94a9a97dee79
BenjAmiNGW <73309308+BenjAmiNGW@users.noreply.github.com>
2020-11-17 16:42:44 +0100

    #5 #6

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,10 +7,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
 
     public function home()
---
+++ src/Controller/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", name="login")
+     */
+    public function login()
+    {
+        return $this->render("user/login.html.twig", []);
+    }
+
+    /**
+     * @Route("/", name="profil")
+     */
+    public function profil()
+    {
+        return $this->render("user/profil.html.twig", []);
+    }
+
+    /**
+     * Symfony gère entièrement cette route
+     * @Route ("/logout", name="logout")
+     */
+    public function logout() {}
+}
