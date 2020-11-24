094ece9aa77592d90f40f8acc9a03ef57649dd32
BenjAmiNGW <73309308+BenjAmiNGW@users.noreply.github.com>
2020-11-24 18:24:12 +0100

    #9 #12

--- src/Form/SortieTypeD.php
+++ src/Form/SortieTypeD.php
@@ -7,17 +7,16 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SortieType extends AbstractType
+class SortieTypeD extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('datedebut')
-            ->add('duree')
-            ->add('datecloture')
-            ->add('nbinscriptionsmax')
-
+            ->add('descriptioninfos')
+            ->add('urlPhoto')
+            ->add('organisateur')
+            ->add('etat')
+            ->add('ville')
         ;
     }
 
