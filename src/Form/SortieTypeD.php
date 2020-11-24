6a8d672ece49e9332a6a163ac7724a1f6c222434
antoinedlp <antdelplanque@gmail.com>
2020-11-24 19:19:34 +0100

    #9

--- src/Form/SortieTypeD.php
+++ src/Form/SortieTypeD.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Sorties;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +17,9 @@ class SortieTypeD extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('descriptioninfos')
-            ->add('urlPhoto')
-            ->add('organisateur')
+            ->add('descriptioninfos', TextareaType::class, array ('label'=>'Description :'))
+            ->add('urlPhoto', UrlType::class, array('label' => 'Lien de la photo :'))
+            ->add('organisateur', IntegerType::class, array('label' => 'Organisateur :'))
             ->add('etat')
             ->add('ville')
         ;
