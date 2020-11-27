6f7cd5786aa90714990fe8d5ee9b8368cd18b2f6
BenjAmiNGW <73309308+BenjAmiNGW@users.noreply.github.com>
2020-11-27 14:53:31 +0100

    no ref

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,14 @@
+function fmmdp() {
+    document.getElementById("mMdp").innerHTML="<form method=\"post\">\n" +
+        "                <div class=\"form-field\">\n" +
+        "                    <label for=\"editpass\">Mot de passe :</label>\n" +
+        "                    <input type=\"password\" id=\"editpass\" name=\"pass\" class=\"form-control\">\n" +
+        "                </div>\n" +
+        "                <div class=\"form-field\">\n" +
+        "                    <label for=\"verifpass\">Confirmer le mot de passe :</label>\n" +
+        "                    <input type=\"password\" id=\"verifpass\" name=\"pass2\" class=\"form-control\">\n" +
+        "                </div>\n" +
+        "            </form>\n";
 
+}
 
